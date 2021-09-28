3da1a01ea26fe9f0bb98d70f6c1e6e9e906d8022
Kade Griffith <kadeg95@gmail.com>
2021-09-28 13:15:16 -0600

    set up the first 4 APIs as listed in MVP. GET POST and PUT all implemented in some form

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,88 @@
 // implement your posts router here
+const express = require('express')
+const Post = require('./posts-model')
+
+const postsRouter = express.Router()
+
+postsRouter.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find()
+        res.status(200).json(posts)
+    }
+    catch {
+       res.status(500).json({
+           message: "The posts information could not be retrieved"
+       }) 
+    }
+})
+
+postsRouter.post('/', async (req, res) => {
+    try {
+        const { title, contents } = req.body
+        if(!title || !contents){
+            res.status(400).json({
+                message: "Please provide title and contents for the post"
+            })
+        }
+        else {
+            const id = await Post.insert(req.body)
+            res.status(201).json({...req.body, id})
+        }
+    }
+    catch {
+       res.status(500).json({
+           message: "There was an error while saving the post to the database"
+       }) 
+    }
+})
+
+postsRouter.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const post = await Post.findById(id)
+        if(!post){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        }
+        else {
+            res.status(200).json(post)
+        }
+    }
+    catch {
+        res.status(500).json({
+            message: "The post information could not be retrieved"
+        }) 
+    }
+})
+
+postsRouter.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { title, contents } = req.body
+        const post = await Post.findById(id)
+
+        if(!title || !contents){
+            res.status(400).json({
+                message: "Please provide title and contents for the post"
+            })
+        }
+        else if(!post){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        }
+        else {
+            await Post.update(id, req.body)
+            const updatedPost = await Post.findById(id)
+            res.status(200).json(updatedPost)
+        }
+    }
+    catch {
+        res.status(500).json({
+            message: "The post information could not be modified"
+        }) 
+    }
+})
+
+module.exports = postsRouter
